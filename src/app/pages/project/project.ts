87e85a3913285c81308812807d5e6a63fd83c346
Furaido28 <ethan.flipo@icloud.com>
2025-11-23 00:51:42 +0100

    Finition de la page d'accueil et ajouts des routes

---
+++ src/app/pages/project/project.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project',
+  standalone: true,
+  imports: [],
+  templateUrl: './project.html',
+  styleUrl: './project.scss',
+})
+export class Project {
+
+}
